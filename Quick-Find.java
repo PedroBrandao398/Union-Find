070c86788d1551f85e539864bc13eedcb7fe6808
Pedro Brandão <pancb@iscte-iul.pt>
2026-05-04 18:19:52 +0100

    Update Quick-Find.java

--- Quick-Find.java
+++ Quick-Find.java
@@ -1,3 +1,24 @@
 public class QuickFindUF {
+  private int id[];
 
+  public QuickFindUF(int N) {
+    id = new id[N]
+    for (int i = 0; i < N, i++) {
+      id[i] = i;
+    }
+  }
+
+  public void union(int p, int q) {
+    int pid = id[p];
+    int qid = id[q];
+    for (int i = 0; i < id.length; i++) {
+      if (id[i] == pid) {
+        id[i] = qid;
+      }
+    }
+  }
+  
+  public boolean connected(int p, int q) {
+    return id[p] == id[q];
+  }
 }
