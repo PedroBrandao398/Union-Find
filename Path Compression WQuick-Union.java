1dd4d552717ce14b943e91162705b1f5eea2dd05
Pedro Brandão <pancb@iscte-iul.pt>
2026-05-05 10:38:14 +0100

    Implement Path Compression in WQuick-Union

--- Path Compression WQuick-Union.java
+++ Path Compression WQuick-Union.java
@@ -1,1 +1,38 @@
+public class PCWQuickUnionUF {
+  private int[] id;
+  private int[] sz;
 
+  public WQuickUnionUF(int N) {
+    id = new int[N];
+    sz = new int[N];
+    for (int i = 0; i < N; i++) {
+      id[i] = i;
+      sz[i] = 1;
+    }
+  }
+
+  private int root(int i) {
+    while (i != id[i]) {
+      id[i] = id[id[i]];
+      i = id[i];
+    }
+    return i;
+  }
+  
+  public void union(int p, int q) {
+    int rp = root(p);
+    int rq = root(q);
+    if (rp == rq) return;
+    if (sz[rp] < sz[rq]) {
+      id[rp] = rq;
+      sz[rq] += sz[rp]; }
+    else { 
+      id[rq] = rp;
+      sz[rp] += sz[rq]; 
+    }
+  }
+  
+  public boolean connected(int p, int q) {
+    return root(p) == root(q);
+  }
+}
