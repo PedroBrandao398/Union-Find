dea35e9d9a015ac2a12120ceb2c133f145a07391
Pedro Brandão <pancb@iscte-iul.pt>
2026-05-05 10:18:01 +0100

    Update Weighted Quick-Union.java

--- Weighted Quick-Union.java
+++ Weighted Quick-Union.java
@@ -1,1 +1,37 @@
+public class QuickUnionUF {
+  private int[] id;
+  private int[] sz;
 
+  public QuickUnionUF(int N) {
+    id = new int[N];
+    sz = new int[N];
+    for (int i = 0; i < N; i++) {
+      id[i] = i;
+      sz[i] = 1;
+    }
+  }
+
+  private int root(int i){
+    while (i != id[i]) {
+      i = id[i];
+    }
+    return i;
+  }
+  
+  public void union(int p, int q) {
+    int rp = root(p);
+    int rq = root(q);
+    if (rp == rq) return;
+    if (sz[rp] < sz[rq]) {
+      id[rp] = rq;
+      sz[rq] += sz[rp]; }
+    else { 
+      id[rq] = rp;
+      sz[rp] += sz[rq]; 
+    }
+  }
+  
+  public boolean connected(int p, int q) {
+    return root(p) == root(q);
+  }
+}
